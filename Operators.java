ef2e292a853c51e4c0a1cdeb22b0710bbe97e634
Imaniephraim <ephraimimani5@gmail.com>
2024-02-23 17:09:55 +0300

    Added variables.java file

---
+++ Operators.java
@@ -0,0 +1,32 @@
+public class Operators {
+    /**
+     * Operators in java
+     *
+     * We have different types of operators in java
+     * 1. Arithmetic operators
+     * Relational operators
+     * Logical operators
+     * Conditional operators
+     *
+     * They follow a certain precedence so that the compiler can know which operator to evaluate first in case multiple operators are used in statement
+     */
+
+    public static void main(Strings[] args){
+        //1. Arithmetic operators
+        //Addition-> +
+        System.out.println(x+y);//returns 26
+
+        //subtraction->-
+        System.out.println(x-y);// returns 14
+
+        //multiplication->*
+        System.out.println(x*y)//returns 120
+
+        //Division->/
+        System.out.println(x/y)//returns 3.33333
+
+        //Modulus->%(Finds the remainder of division)
+        // System.out.println(x%y)//returns 2
+
+    }
+}
