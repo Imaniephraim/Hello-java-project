7bd4b3d7e65f959c6fd8b025436f654bf7a48ce4
Imaniephraim <160492164+Imaniephraim@users.noreply.github.com>
2024-03-05 15:43:21 +0300

    done

---
+++ StudentRecord.java
@@ -0,0 +1,34 @@
+public class StudentRecord {
+    //Instance variables
+    private String name;
+    private String address;
+    private int age;
+    private double mathGrade;
+    private double englishGrade;
+    private double scienceGrade;
+    private double average;
+
+    //static/class variable(s)
+    private static int studentCount;
+
+
+    //we'll add more code here
+    //where:
+    //private here means that the variables are only accessible within the class. Other objects can't access these variables directly
+
+    //methods
+    //To declare methods we write
+
+    //<modifier> <returnType> <name> (<parameter>*) {
+    //   <statement>
+    // }
+
+    //Where
+    //<modifier> - can carry a number of different modifiers
+    // <returnType> - can be any data type (including void)
+    //<name> - can be any valid identifier
+    // <Parameter> - <parameter_type><parameter_name>
+
+    // Accessor
+
+}
---
+++ oop.java
@@ -0,0 +1,59 @@
+public class oop {
+    /** Creating your own classes
+     *
+     * At the end of this lecture you should be able to :
+     *
+     * -> Create your own classes
+     * ->Declare properties (fields) and methods(actions) for your classes
+     *->Use the 'this' keyword to access instance data
+     * ->Create and call overloaded methods
+     * ->Use access modifiers to control access to class members
+     *
+     * Defining Your Own Class
+     * In java, a class is a blueprint or template for creating objects
+     * It defines the structure  and behaviour of objects
+     * They encapsulate data(attributes)and methods(functions) that operate on that data.
+     * Encapsulation involves bundling the data(attributes0 amd methods(function) together within a class
+     * Encapsulation helps in hiding the internal implementation details of a class and exposing only necessary functionalities through well-defined
+     * It also ensures data integrity and improves code maintainability by restricting direct access to class members from outside the class
+     * Methods(also called member functions) define the behaviour of an object.They perform operations on the objects data or provide functionality
+     * both attributes and methods are declared within the class definition.
+     *
+     */
+
+    //Things to take note in the syntax of this session
+    // * -> means that there maybe 0 or more occurrences of the line where it was applied
+    //<description> indicates that you have to substitute an actual value of this part instead of typing it as it is.
+    //[] -> indicates that this part s optional
+
+    //To define a class, we write
+
+    //<modifiers> class <name> {
+    //<attributers
+
+
+
+
+
+
+
+
+    //instance variables
+    //Declaring properties (attributes)
+    //To declare a certain attribute for our class, we enter
+
+    //<modifier> <Type> <name> [default_value];
+
+    //Coding guidelines
+    //i. declare all instance variable right after "public" class
+    //ii. Declare one variable for each line
+    //iii. Use an appropriate data type for each variable you declare
+    //
+
+
+
+
+
+
+}
+
